d9f837b80751b6330d097d45a02f7e0c450d534e
Maaam12 <suneater512@gmail.com>
2022-11-27 23:11:53 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   lib/TabbarCoffee/cappuccino.dart
    	modified:   lib/TabbarCoffee/espresso.dart
    	modified:   lib/TabbarCoffee/latte.dart
    	modified:   lib/TabbarCoffee/mocha.dart

--- lib/TabbarCoffee/espresso.dart
+++ lib/TabbarCoffee/espresso.dart
@@ -27,106 +27,110 @@ class Espresso extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: InkWell(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => CoffeeDetailPage()));
-        },
-        child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          itemCount: images.length,
-          itemBuilder: (context, index) {
-            return Row(
-              children: [
-                Container(
-                  height: 250,
-                  width: 160,
-                  child: Column(
-                    children: [
-                      SizedBox(height: 10,),
-                      Container (
-                        height: 135,
-                        width: 140,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                            image: AssetImage(images[index]),
-                            fit: BoxFit.cover,
-                          )
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Espresso",
-                              style: TextStyle(color: Colors.black),),
-                            SizedBox(
-                              height: 3,
-                              ),
-                            Text(
-                              ingredients[index], 
-                              style: TextStyle(color: Colors.black, fontSize: 11),
+    return Column(
+      children: [
+        Flexible(
+          child: InkWell(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => CoffeeDetailPage()));
+            },
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: images.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Container(
+                      height: 250,
+                      width: 160,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 10,),
+                          Container (
+                            height: 135,
+                            width: 140,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                image: AssetImage(images[index]),
+                                fit: BoxFit.cover,
+                              )
                             ),
-                            SizedBox(height: 5,),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                  r'$',
-                                  style: TextStyle(
-                                    color: Colors.deepOrangeAccent,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,),
-                                ),
                                 Text(
-                                  "${price[index]}",
-                                  style: TextStyle(
-                                    color: Colors.deepOrangeAccent,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,),
+                                  "Espresso",
+                                  style: TextStyle(color: Colors.black),),
+                                SizedBox(
+                                  height: 3,
+                                  ),
+                                Text(
+                                  ingredients[index], 
+                                  style: TextStyle(color: Colors.black, fontSize: 11),
                                 ),
+                                SizedBox(height: 5,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text(
+                                      r'$',
+                                      style: TextStyle(
+                                        color: Colors.deepOrangeAccent,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,),
+                                    ),
+                                    Text(
+                                      "${price[index]}",
+                                      style: TextStyle(
+                                        color: Colors.deepOrangeAccent,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,),
+                                    ),
+                                      ],
+                                    ),
+                                    Container(
+                                  height: 38,
+                                  width: 38,
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 28,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: Colors.deepOrangeAccent,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                )
                                   ],
                                 ),
-                                Container(
-                              height: 38,
-                              width: 38,
-                              child: Icon(
-                                Icons.add,
-                                color: Colors.white,
-                                size: 28,
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.deepOrangeAccent,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            )
+                                
                               ],
                             ),
-                            
-                          ],
-                        ),
+                          ),
+                          
+                        ],
                       ),
-                      
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(20),
-                  ), 
-                ),  
-                SizedBox(
-                  width: 20,
-                )
-              ],
-            ); 
-          }),
-      ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(20),
+                      ), 
+                    ),  
+                    SizedBox(
+                      width: 20,
+                    )
+                  ],
+                ); 
+              }),
+          ),
+        ),
+      ],
     );
   }
 }
